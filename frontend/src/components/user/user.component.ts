6241e487f354e1f05b4b8cc055868abfdc2f9ac1
Rohit8329 <rohitmg148@gmail.com>
2025-09-26 13:47:55 +0000

    updated files

--- frontend/src/components/user/user.component.ts
+++ frontend/src/components/user/user.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrls: ['./user.component.css']
 })
 export class UserComponent {
+  userForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      console.log('Form Submitted!', this.userForm.value);
+      // You can add your submission logic here
+    } else {
+      console.log('Form is invalid');
+      this.userForm.markAllAsTouched();
+    }
+  }
 }
